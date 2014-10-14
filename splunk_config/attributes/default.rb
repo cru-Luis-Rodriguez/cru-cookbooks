e391636846eddf8cdeab63d489614d4b0f61845b
luis rodriguez <luis.rodriguez@cru.org>
2014-10-14 12:03:10 -0400

    modified attributes

--- splunk_config/attributes/default.rb
+++ splunk_config/attributes/default.rb
@@ -4,10 +4,10 @@ default['splunk']['install_path']='/opt/splunkforwarder'
 
 #Use json on Opsworks Stack setting to provided attribute information. Attributes can not be nil
 
-default['splunk']['pass']='nil'
-default['splunk']['newpass']='nil'
-default['splunk']['forward_server']='nil'
-default['splunk']['receiver_port']='nil'
-default['splunk']['auth']='nil'
-default['splunk']['monitor_path']='nil'
+default['splunk']['pass']='changeme'
+default['splunk']['newpass']='default'
+default['splunk']['forward_server']='default'
+default['splunk']['receiver_port']='9997'
+default['splunk']['auth']='admin'
+default['splunk']['monitor_path']='default'
 default['splunk']['group']='default'
