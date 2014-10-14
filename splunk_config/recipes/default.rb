09b6f2b247efe20e10bcd94f0673b5ddf99a737f
luis rodriguez <luis.rodriguez@cru.org>
2014-10-14 10:57:47 -0400

    modified change password code

--- splunk_config/recipes/default.rb
+++ splunk_config/recipes/default.rb
@@ -48,7 +48,9 @@ bash 'change-admin-user-password-from-default' do
     if !File.exists?("#{splunk_dir}/etc/.setup_#{user}_password")
     user "root"
     cwd "#{splunk_dir}/bin"
-    code <<-EOH ./splunk edit user #{user} -password #{newpass} -auth #{user}:#{pass} EOH
+    code <<-EOH
+    ./splunk edit -user #{user} -password #{newpass} -auth #{user}:#{pass}
+    EOH
     end
 end
 
