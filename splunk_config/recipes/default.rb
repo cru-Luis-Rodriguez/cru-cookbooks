3355731416b877fba9d9bab0c04b3c3bd684c42d
luis rodriguez <luis.rodriguez@cru.org>
2014-10-14 11:23:02 -0400

    fix

--- splunk_config/recipes/default.rb
+++ splunk_config/recipes/default.rb
@@ -49,7 +49,7 @@ bash 'change-admin-user-password-from-default' do
     user "root"
     cwd "#{splunk_dir}/bin"
     code <<-EOH
-    ./splunk edit -user #{user} -password #{newpass} -auth #{user}:#{pass}
+    ./splunk edit user #{user} -password #{newpass} -auth #{user}:#{pass}
     EOH
     end
 end
